b4ba9238b7fa87bc15cf2c96837941d75ffc4622
hub99-h <hiba.lol78@gmail.com>
2026-04-18 11:14:10 +0100

    initial project setup

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from backend');
+});
+
+module.exports = app;
+
+if (require.main === module) {
+  app.listen(5000, () => console.log('Server running on port 5000'));
+}
