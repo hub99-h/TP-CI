30f5776e8f4fe294294c945e1d83d3dd4aa7b7a0
hub99-h <hiba.lol78@gmail.com>
2026-04-28 23:29:31 +0100

    fix eslint config backend

---
+++ backend/eslint.config.js
@@ -0,0 +1,15 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        require: "readonly",
+        module: "readonly",
+        process: "readonly",
+        __dirname: "readonly",
+      }
+    }
+  }
+];
